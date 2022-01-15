3f71057ed9a563318216b59656a13f5880c2d192
Vasileios Roussos <v.roussos.insurance@gmail.com>
2022-01-15 16:45:30 +0200

    Attiki Odos changes

--- TheHighway/attiki_odos.cpp
+++ TheHighway/attiki_odos.cpp
@@ -1,4 +1,7 @@
 #include "attiki_odos.h"
 
-attiki_odos::attiki_odos(const int in_NSegs) :
-	NSegs(in_NSegs), nodes(in_NSegs+1), segments(in_NSegs){}
+attiki_odos::attiki_odos(const int in_NSegs, const int in_K, const int in_Percent) :
+	NSegs(in_NSegs), K(in_K), Percent(in_Percent), segments(in_NSegs), no_of_vehicles(in_NSegs*in_K)
+{
+	cout << "Highway operating..\n";
+}
--- TheHighway/attiki_odos.h
+++ TheHighway/attiki_odos.h
@@ -9,10 +9,12 @@ using namespace std;
 class attiki_odos
 {
 	const int NSegs;
-	vector <string> nodes;
+	const int K;
+	const int Percent;
 	vector <segment> segments;
+	int no_of_vehicles;
 
-	attiki_odos(const int in_NSegs);
+	attiki_odos(const int in_NSegs, const int in_K, const int in_Percent);
 
 };
 
